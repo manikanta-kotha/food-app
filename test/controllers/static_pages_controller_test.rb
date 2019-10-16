34079a2bf86f8de00a58c4e4ef0ba06194c45242
Manikantae <nagamanikanta.satyanarayana@freshworks.com>
2019-10-16 16:29:34 +0530

    genric helper function added for creating titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,16 +9,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_select "title", "help | Ruby on Rails Tutorial Sample App"
     assert_response :success
   end
 
   test "should get about" do
     get static_pages_about_url
-    assert_select "title", "about"
+    assert_select "title", "about | Ruby on Rails Tutorial Sample App"
     assert_response :success
   end
 end
