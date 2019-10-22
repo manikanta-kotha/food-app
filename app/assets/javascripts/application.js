f4401dfbbfdae51fb5e86df51419ecdc868d3a9f
Manikantae <nagamanikanta.satyanarayana@freshworks.com>
2019-10-22 11:09:31 +0530

    added to localstorage helpers to store cart items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,25 @@
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap
+
+const CHECKOUT_CART_PATH = 'cart'
+
+$(document).ready(function() {
+  let cartData = localStorage.setItem(CHECKOUT_CART_PATH, '[]');
+  $('.cart-button').click(function(event){
+    event.preventDefault();
+    const element = $(this)
+    const productData =  element.attr("data-product")
+    if (productData) {
+      // store this product details
+      const productDetails = JSON.parse(productData)
+      element.removeClass('cart-button')
+      element.text('Added')
+      element.prop('disabled', true);
+      const cartDetails = JSON.parse(localStorage.getItem(CHECKOUT_CART_PATH));
+      cartDetails.push(productDetails);
+      localStorage.setItem(CHECKOUT_CART_PATH, JSON.stringify(cartDetails))
+      console.log('cartDetails', cartDetails);
+    }
+  });
+})
